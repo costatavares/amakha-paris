97d5d921d607670fe402dec72deffd50407e0b6e
costatavares <costatavares222@gmail.com>
2020-05-20 17:10:58 -0300

    Segundo Commit migrates

---
+++ database/migrations/2020_05_20_200652_create_box_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoxProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('box_products', function (Blueprint $table) {
+            $table->integer('idProduct');
+            $table->integer('idBox');
+            $table->timestamps();
+            $table->index('idProduct');
+            $table->index('idBox');
+            $table->foreign('idProduct')->references('idProduct')->on('products')->onDelete('cascade');
+            $table->foreign('idBox')->references('idBox')->on('boxes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('box_products');
+    }
+}
